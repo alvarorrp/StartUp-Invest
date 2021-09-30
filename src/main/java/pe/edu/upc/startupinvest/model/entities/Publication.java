db54f4eb4f6645a2df853607e65cb0c636f6c9de
alvarorrp <alvarorrp2000@gmail.com>
2021-09-30 06:36:23 -0500

    version-1.0.9-tp
    funcional

--- src/main/java/pe/edu/upc/startupinvest/model/entities/Publication.java
+++ src/main/java/pe/edu/upc/startupinvest/model/entities/Publication.java
@@ -2,7 +2,7 @@ package pe.edu.upc.startupinvest.model.entities;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
